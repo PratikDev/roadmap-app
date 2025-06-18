3353d30bbdfd24c882840a6366a92d789f0ea66c
Pratik Dev <pratikdevofficial1@gmail.com>
2025-06-18 23:31:25 +0600

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+const AUTH_ROUTES = ["/sign-in", "/sign-up"];
+
+export async function middleware(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+	const isAuthRoute = AUTH_ROUTES.includes(request.nextUrl.pathname);
+
+	// 1. If request is for auth route and session exists, redirect to home
+	if (isAuthRoute && sessionCookie) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+	// 2. If request is not for auth route and session does not exist, redirect to sign-in
+	if (!isAuthRoute && !sessionCookie) {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
+
+	// Otherwise, continue with the request
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+		 */
+		"/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+	],
+};
